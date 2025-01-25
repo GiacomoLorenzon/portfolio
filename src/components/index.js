3c29dfa832895efb7251630aadb0d1518cb7c330
Giacomo Lorenzon <giacomo.lorenzon@mail.polimi.it>
2025-01-25 01:07:18 +0100

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Main } from "./Main";
-export { default as History } from "./History";
-export { default as Research } from "./Research";
-export { default as Footer } from "./Footer";
